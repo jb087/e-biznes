2afd2f0e6c31a1d881d3653c050f42fd38d3e073
jb087 <jb087@op.pl>
2020-05-21 19:42:11 +0200

    # Added github auth button

--- e_biznes_react/src/components/NavigationBar.js
+++ e_biznes_react/src/components/NavigationBar.js
@@ -1,34 +1,52 @@
-import React, {Component} from 'react';
+import React, {useContext} from 'react';
 import logo from '../logo-e-biznes.png'
 import '../css/NavigationBar.css'
 import SocialLoginButton from "./SocialLoginButton";
+import {UserContext} from "../providers/UserProvider";
+import {signOut} from "../services/AuthService"
 
-class NavigationBar extends Component {
+export default function NavigationBar({hideBasket, showBasket}) {
+    const {user, setUser} = useContext(UserContext);
 
-    render() {
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <div className={"Logo"} onClick={this.props.hideBasket}>
-                    <img
-                        src={logo}
-                        alt="logo"
-                        className="d-inline-block align-top logo mr-4"
-                    />
-                </div>
-                <form className="form-inline">
-                    <button
-                        className="btn btn-outline-primary my-2 my-sm-0"
-                        type="button"
-                        onClick={() => this.props.showBasket()}
-                    >
-                        Basket
-                    </button>
-                    <SocialLoginButton provider={"google"} title={"Login with Google"}/>
-                    <SocialLoginButton provider={"github"} title={"Login with GitHub"}/>
-                </form>
-            </nav>
-        );
-    }
+    const handleSignOut = () => {
+        signOut(user);
+        setUser(null);
+    };
+
+    return (
+        <nav className="navbar navbar-light bg-light">
+            <div className={"Logo"} onClick={hideBasket}>
+                <img
+                    src={logo}
+                    alt="logo"
+                    className="d-inline-block align-top logo mr-4"
+                />
+            </div>
+            <form className="form-inline">
+                <button
+                    className="btn btn-outline-primary my-2 my-sm-0 mr-2"
+                    type="button"
+                    onClick={() => showBasket()}
+                >
+                    Basket
+                </button>
+                {
+                    user ? (
+                        <button
+                            className="btn btn-outline-danger my-2 my-sm-0 mr-2"
+                            type="button"
+                            onClick={handleSignOut}
+                        >
+                            Logout
+                        </button>
+                    ) : (
+                        <div>
+                            <SocialLoginButton provider={"google"} title={"Login with Google"}/>
+                            <SocialLoginButton provider={"github"} title={"Login with GitHub"}/>
+                        </div>
+                    )
+                }
+            </form>
+        </nav>
+    );
 }
-
-export default NavigationBar;
--- e_biznes_react/src/services/AuthService.js
+++ e_biznes_react/src/services/AuthService.js
@@ -1,9 +1,10 @@
 const host = "http://localhost:9000/";
+const authenticatePath = host + "authenticate/";
+const signOutPath = host + "api/signOut";
 
 export async function authenticate(provider, queryParams) {
     return fetch(
-        host + "authenticate/" + provider + "?" + queryParams,
-        {
+        authenticatePath + provider + "?" + queryParams, {
             method: "GET",
             credentials: 'include',
             headers: {
@@ -27,4 +28,17 @@ export async function authenticate(provider, queryParams) {
             console.log('Error on social auth');
             console.log(response);
         });
+}
+
+export function signOut(user) {
+    fetch(signOutPath, {
+        method: "GET",
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'http://localhost:3000',
+            'X-Auth-Token': user?.token
+        }
+    });
 }
