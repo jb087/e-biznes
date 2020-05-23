7356a4363959af105ec6d2c3d948d0916635078e
jb087 <jb087@op.pl>
2020-05-23 14:32:01 +0200

    # Skeleton of admin panel auth

--- e_biznes_scala_play/app/controllers/HomeController.scala
+++ e_biznes_scala_play/app/controllers/HomeController.scala
@@ -1,7 +1,7 @@
 package controllers
 
 import com.mohiva.play.silhouette.api.actions.SecuredRequest
-import com.mohiva.play.silhouette.api.{LogoutEvent, Silhouette}
+import com.mohiva.play.silhouette.api.{HandlerResult, LogoutEvent, Silhouette}
 import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
 import javax.inject._
 import play.api.Environment
@@ -23,8 +23,20 @@ class HomeController @Inject()(components: ControllerComponents,
                                authInfoRepository: AuthInfoRepository)
                               (implicit ec: ExecutionContext) extends AbstractController(components) with I18nSupport {
 
-  def index: Action[AnyContent] = Action {
-    Ok(views.html.index())
+  def index: Action[AnyContent] = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    Future.successful(Ok(views.html.index()))
+  }
+
+  def signOut: Action[AnyContent] = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
+    silhouette.env.eventBus.publish(LogoutEvent(request.identity, request))
+    silhouette.env.authenticatorService.discard(request.authenticator, Ok)
+  }
+
+  def signIn: Action[AnyContent] = silhouette.UserAwareAction.async { implicit request =>
+    request.identity match {
+      case Some(user) => Future.successful(Redirect(routes.HomeController.index()))
+      case None => Future.successful(Ok(views.html.signIn()))
+    }
   }
 
   def reactApp(path: String): Action[AnyContent] = TODO
@@ -43,10 +55,5 @@ class HomeController @Inject()(components: ControllerComponents,
         new Status(r.status)(r.bodyAsBytes).as(r.contentType)
       }
     }
-  }
-
-  def signOut: Action[AnyContent] = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
-    silhouette.env.eventBus.publish(LogoutEvent(request.identity, request))
-    silhouette.env.authenticatorService.discard(request.authenticator, Ok)
   }
 }
