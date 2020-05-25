d28f4d8b2cbe7fbb15447dde4bec802d0e648254
jb087 <jb087@op.pl>
2020-05-25 13:50:31 +0200

    # Added possibility to ad opinion

--- e_biznes_react/src/services/OpinionsService.js
+++ e_biznes_react/src/services/OpinionsService.js
@@ -1,9 +1,12 @@
+import {check400Status} from "../utils/RequestUtils";
+
 const host = "http://localhost:9000/";
 const apiPath = host + "api/";
-const opinionsByProductId = apiPath + "opinions/";
+const opinionsByProductIdPath = apiPath + "opinions/";
+const createOpinionPath = apiPath + "opinion";
 
 export async function getOpinionsByProductId(productId) {
-    return fetch(opinionsByProductId + productId, {
+    return fetch(opinionsByProductIdPath + productId, {
         method: "GET",
         headers: {
             'Accept': 'application/json',
@@ -11,5 +14,20 @@ export async function getOpinionsByProductId(productId) {
             'Access-Control-Allow-Origin':'http://localhost:3000'
         }
     })
+        .then(response => check400Status(response))
         .then(response => response.json());
+}
+
+export function createOpinion(opinion) {
+    return fetch(createOpinionPath, {
+        method: "POST",
+        body: JSON.stringify(opinion),
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'http://localhost:3000'
+        }
+    })
+        .then(response => check400Status(response))
+        .then(response => alert("Opinion added!"))
 }
