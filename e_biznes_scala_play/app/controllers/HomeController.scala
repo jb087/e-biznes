5f683f2b303cfc131399b691cbc7ca3cc83ae42d
jb087 <jb087@op.pl>
2020-05-30 19:06:39 +0200

    # Code refactor # Deleted redirect to react app

--- e_biznes_scala_play/app/controllers/HomeController.scala
+++ e_biznes_scala_play/app/controllers/HomeController.scala
@@ -1,18 +1,17 @@
 package controllers
 
 import com.mohiva.play.silhouette.api.actions.SecuredRequest
-import com.mohiva.play.silhouette.api.{LogoutEvent, Silhouette}
 import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.api.{LogoutEvent, Silhouette}
 import javax.inject._
 import play.api.Environment
-import play.api.http.ContentTypes
 import play.api.i18n.I18nSupport
 import play.api.libs.ws.WSClient
 import play.api.mvc._
 import services.IndexRenderService
 import utils.auth.DefaultEnv
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 @Singleton
 class HomeController @Inject()(components: ControllerComponents,
@@ -25,24 +24,6 @@ class HomeController @Inject()(components: ControllerComponents,
 
   def index: Action[AnyContent] = Action {
     Ok(views.html.index())
-  }
-
-  def reactApp(path: String): Action[AnyContent] = TODO
-
-    /*silhouette.UserAwareAction.async { implicit request =>
-    fetchWebpackServer(path)
-  }*/
-
-  private def fetchWebpackServer(path: String)(implicit request: RequestHeader): Future[Result] = {
-    ws.url(s"http://localhost:3000/$path").get().map { r =>
-      if (r.contentType.equalsIgnoreCase(HTML(Codec.utf_8))) {
-        val html = r.bodyAsBytes.utf8String
-
-        Ok(indexRenderService.setCsrfToken(html)).as(ContentTypes.HTML)
-      } else {
-        new Status(r.status)(r.bodyAsBytes).as(r.contentType)
-      }
-    }
   }
 
   def signOut: Action[AnyContent] = silhouette.SecuredAction.async { implicit request: SecuredRequest[DefaultEnv, AnyContent] =>
